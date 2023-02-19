ab735fd1597d1cdf51482aa2812bc8abc990d038
Ac-Arreola <120034358@upq.edu.mx>
2023-02-19 03:37:44 -0600

    modal's cli-aux F agregar y consultar tickets

--- resources/views/modalAgregarT.blade.php
+++ resources/views/modalAgregarT.blade.php
@@ -51,16 +51,9 @@
                 <div class="row mb-3">
                   <label for="inputFecha" class="col-sm-2 col-form-label"> Fecha : </label>
                   <div class="col-sm-10">
-    
-                      <div class="flex items-center justify-center">
-                        <div class="datepicker relative form-floating mb-3 xl:w-96">
-                          <input type="text"
-                            class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                            placeholder="Select a date" />
-                          <label for="floatingInput" class="text-gray-700">Select a date</label>
-                        </div>
-                      </div>
-
+                      
+                      <input
+                        id="fechaAdd" type="date" name="txtfechaAdd" class="form-control"/>
                   </div>
                 </div>
               {{--Clasificación--}}
