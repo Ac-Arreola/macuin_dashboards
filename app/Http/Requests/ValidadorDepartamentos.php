bbb6d39b23f02b7f570cf5a33bf445d8d6196f6b
Ac-Arreola <120034358@upq.edu.mx>
2023-02-22 00:13:56 -0600

    Union crud usuarios

--- app/Http/Requests/ValidadorDepartamentos.php
+++ app/Http/Requests/ValidadorDepartamentos.php
@@ -24,7 +24,7 @@ class ValidadorDepartamentos extends FormRequest
     public function rules()
     {
         return [
-            
+            'txtNombre'=> 'required', 
         ];
     }
 }
---
+++ resources/views/Jefe/eliminarUsuario.blade.php
@@ -0,0 +1,28 @@
+<!-- Modal -->
+<div class="modal fade" id="eliminarUsuario-{{$consulta->id_usu}}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="eliminarUsuario" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content" id="modal">
+        <form class="m-4" method="post" action="{{route('usuario.destroy',$consulta->id_usu)}}">
+          @csrf
+
+          {!!method_field('DELETE')!!}
+
+
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="staticBackdropLabel">¿Seguro que deseas Eliminar?</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+
+        <div class="modal-body">
+          <h5 class="card-title">{{$consulta->Nombre}}</h5>
+              
+        </div>
+
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+          <button type="submit" class="btn btn-primary">Eliminar</button>
+        </form>
+        </div>
+      </div>
+    </div>
+  </div>
